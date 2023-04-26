a4310ca0963d1917325c55c61f08b6a98e381526
tiendat <dat.tran@pascal.studio>
2023-04-26 22:24:35 +0700

    Require geolocation in invitation page

--- src/pages/invitation/[id].vue
+++ src/pages/invitation/[id].vue
@@ -3,11 +3,15 @@ import { useHead } from '@vueuse/head'
 import { useRoute } from 'vue-router'
 import { useViewWrapper } from '/@src/stores/viewWrapper'
 import { useInvitationStore } from '/@src/stores/invitations'
+import { useGeolocation } from '@vueuse/core'
 
 const viewWrapper = useViewWrapper()
 viewWrapper.setPageTitle('Invitation')
 const invitationStore = useInvitationStore()
 const route = useRoute()
+const { coords, resume } = useGeolocation()
+
+const currentPosition = ref<{ longitude: number; latitude: number } | null>(null)
 
 useHead({
   title: 'Invitation - My app',
@@ -17,14 +21,28 @@ const getInvitationInfo = computed(() => {
   return invitationStore.getInvitationInfo
 })
 
-onMounted(() => {
+watch(coords, (newPosition) => {
+  console.log('current position change -> ', { newPosition })
   const invitationCode = route.params.id
-  invitationStore.fetchInvitationInfo(invitationCode)
+  if (newPosition.latitude && newPosition.longitude && invitationCode) {
+    currentPosition.value = newPosition
+    invitationStore.fetchInvitationInfo(invitationCode)
+  }
+})
+
+onMounted(() => {
+  console.log('invitation[id].vue -> mounted')
+  resume()
 })
 </script>
 
 <template>
   <div class="page-content-inner">
-    <Invitation v-if="getInvitationInfo?._id" :invitation-info="getInvitationInfo" />
+    <Invitation
+      v-if="
+        currentPosition?.latitude && currentPosition?.longitude && getInvitationInfo?._id
+      "
+      :invitation-info="getInvitationInfo"
+    />
   </div>
 </template>
